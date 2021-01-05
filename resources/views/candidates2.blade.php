093c5e959bf6a835cf69c5ac3dcbc6b2d40f285b
bryanleona <74999675+bryanleona@users.noreply.github.com>
2021-01-05 16:33:06 +0700

    commit

--- resources/views/candidates2.blade.php
+++ resources/views/candidates2.blade.php
@@ -9,23 +9,23 @@
     <div class="container mt-4">
         <div class="row mb-3">
             <img src="https://via.placeholder.com/250x250.png" alt="candidatePic" class="mr-4">
-            <div class="mt-2">
+            <div class="mt-2" style="width: 70%">
                 <h3>Bowo</h3>
                 <p>Lahir : 16-09-1989</p>
                 <p>Asal : Yogyakarta</p>
                 <p>Partai : Persatuan Indonesia</p>
-                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga optio cupiditate obcaecati doloribus totam perferendis deserunt adipisci eos placeat est saepe exercitationem tempora explicabo eaque eum aspernatur, repellat odio inventore!</p>
             </div>
         </div>
 
         <div class="row mb-3">
             <img src="https://via.placeholder.com/250x250.png" alt="candidatePic" class="mr-4">
-            <div class="mt-2">
+            <div class="mt-2" style="width: 70%">
                 <h3>David</h3>
                 <p>Lahir : 26-10-1979</p>
                 <p>Asal : Batam</p>
                 <p>Partai : Persatuan Indonesia</p>
-                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga optio cupiditate obcaecati doloribus totam perferendis deserunt adipisci eos placeat est saepe exercitationem tempora explicabo eaque eum aspernatur, repellat odio inventore!</p>
             </div>
         </div>
     </div>
--- resources/views/candidates3.blade.php
+++ resources/views/candidates3.blade.php
@@ -9,23 +9,23 @@
     <div class="container mt-4">
         <div class="row mb-3">
             <img src="https://via.placeholder.com/250x250.png" alt="candidatePic" class="mr-4">
-            <div class="mt-2">
+            <div class="mt-2" style="width: 70%">
                 <h3>Bob</h3>
                 <p>Lahir : 28-11-1989</p>
                 <p>Asal : Tanggerang</p>
                 <p>Partai : Demokrasi Pancasila</p>
-                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga optio cupiditate obcaecati doloribus totam perferendis deserunt adipisci eos placeat est saepe exercitationem tempora explicabo eaque eum aspernatur, repellat odio inventore!</p>
             </div>
         </div>
 
         <div class="row mb-3">
             <img src="https://via.placeholder.com/250x250.png" alt="candidatePic" class="mr-4">
-            <div class="mt-2">
+            <div class="mt-2" style="width: 70%">
                 <h3>Putu</h3>
                 <p>Lahir : 26-09-1979</p>
                 <p>Asal : Bekasi</p>
                 <p>Partai : Demokrasi Pancasila</p>
-                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Biodata : Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga optio cupiditate obcaecati doloribus totam perferendis deserunt adipisci eos placeat est saepe exercitationem tempora explicabo eaque eum aspernatur, repellat odio inventore!</p>
             </div>
         </div>
     </div>
